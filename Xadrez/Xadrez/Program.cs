2ce209c4d51e3729722e098de385a3a0f058d204
EliezerMoura <eliezer.newmemx003@gmail.com>
2018-09-25 18:06:13 -0300

    Aula 75

--- Xadrez/Xadrez/Program.cs
+++ Xadrez/Xadrez/Program.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xadrez.tabuleiro;
 
 namespace Xadrez {
     class Program {
         static void Main(string[] args) {
 
+            Posicao P;
         }
     }
 }
--- Xadrez/Xadrez/tabuleiro/Posicao.cs
+++ Xadrez/Xadrez/tabuleiro/Posicao.cs
@@ -6,5 +6,16 @@ using System.Threading.Tasks;
 
 namespace Xadrez.tabuleiro {
     class Posicao {
+        public int Linha { get; set; }
+        public int Coluna { get; set; }
+
+        public Posicao(int linha, int coluna) {
+            Linha = linha;
+            Coluna = coluna;
+        }
+
+        public override string ToString() {
+            return Linha + ", " + Coluna;
+        }
     }
 }
