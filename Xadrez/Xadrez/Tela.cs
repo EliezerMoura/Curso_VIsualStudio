2fe21c978036fd181e48b861579663f896c9e2b2
EliezerMoura <eliezer.newmemx003@gmail.com>
2018-09-26 14:26:37 -0300

    Aula 82

--- Xadrez/Xadrez/Tela.cs
+++ Xadrez/Xadrez/Tela.cs
@@ -11,14 +11,29 @@ namespace Xadrez {
         public static void ImprimirTabuleiro(Tabuleiro tab) {
 
             for (int i = 0; i < tab.Linhas; i++) {
+                Console.Write(8 - i + " ");
                 for (int j = 0; j < tab.Colunas; j++) {
                     if (tab.peca(i, j) == null)
                         Console.Write(" -");
-                    else
-                        Console.Write(" " + tab.peca(i, j));
+                    else {
+                        Console.Write(" ");
+                        ImprimirPeca(tab.peca(i, j));
+                    }
                 }
                 Console.WriteLine();
-            }            
+            }
+            Console.Write("\n   a b c d e f g h");
+        }
+
+        public static void ImprimirPeca(Peca peca) {
+            if (peca.Cor == Cor.Branco)
+                Console.Write(peca);
+            else {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
+            }
         }
     }
 }
