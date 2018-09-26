484a11dc78e313b4a06fe28945a8d01ae0747fe2
EliezerMoura <eliezer.newmemx003@gmail.com>
2018-09-26 11:12:51 -0300

    Aula 79

--- Xadrez/Xadrez/tabuleiro/Tabuleiro.cs
+++ Xadrez/Xadrez/tabuleiro/Tabuleiro.cs
@@ -20,9 +20,29 @@ namespace Xadrez.tabuleiro {
             return pecas[linha, coluna];
         }
 
+        public Peca peca(Posicao pos) {
+            return pecas[pos.Linha, pos.Coluna];
+        }
+
         public void colocarPeca(Peca p, Posicao pos) {
             pecas[pos.Linha, pos.Coluna] = p;
             p.posicao = pos;
+        }
+
+        public bool posicaoValida(Posicao pos) {
+            if ((pos.Linha < 0) || (pos.Coluna < 0) || (pos.Linha >= Linhas) || (pos.Coluna >= Colunas))
+                return false;
+            return true;
+        }
+
+        public void validarPosicao(Posicao pos) {
+            if (!posicaoValida(pos))
+                throw new TabuleiroException("Posição inválida");
+        }
+
+        public bool existePeca(Posicao pos) {
+            validarPosicao(pos);
+            return peca(pos) != null;
         }
     }
 }
