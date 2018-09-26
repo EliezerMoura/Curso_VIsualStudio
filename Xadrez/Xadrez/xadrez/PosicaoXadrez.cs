c034d04a5eb6a7c53772f648045315ff1d66f8cc
EliezerMoura <eliezer.newmemx003@gmail.com>
2018-09-26 14:10:18 -0300

    Aula 81

---
+++ Xadrez/Xadrez/xadrez/PosicaoXadrez.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xadrez.tabuleiro;
+
+namespace Xadrez.xadrez {
+    class PosicaoXadrez {
+        public int Linha { get; set; }
+        public char Coluna { get; set; }
+
+        public PosicaoXadrez(char coluna, int linha) {
+            Coluna = coluna;
+            Linha = linha;
+        }
+        
+        public Posicao toPosicao() {
+            return new Posicao(8 - Linha, Coluna - 'a');
+        }
+
+        public override string ToString() {
+            return "" + Coluna + Linha;
+        }
+    }
+}
